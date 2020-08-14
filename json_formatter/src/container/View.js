01ec8fd2bc44011f4b1c224287599134f72af1bf
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-08-14 08:39:14 +0530

    Passing Data from Input to view

--- json_formatter/src/container/View.js
+++ json_formatter/src/container/View.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import styles from "./View.module.css";
@@ -6,34 +6,46 @@ import TextField from '@material-ui/core/TextField';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import { Button } from '@material-ui/core';
 
-class View extends Component {
-    render() {
-        return (
-            <>
-                <Grid container spacing={1} direction="row"
-                    justify="space-around"
-                    alignItems="stretch">
-                    <Grid item xs={4} spacing={1} className={styles.GridRoot1}>
-                        <Paper className={styles.PaperStyle}><br />
-                            <TextField className={styles.InputStyle} id="outlined-basic" label="Raw JSON" variant="outlined" multiline="true" />
-                            {/* <TextareaAutosize aria-label="minimum height" className={styles.InputStyle} rowsMin={35} placeholder="Minimum 3 rows" /> */}
+const View = () => {
 
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={4} spacing={1} className={styles.GridRoot2}>
-                        <Paper className={styles.PaperStyle} style={{ textAlign: "center", justifyContent: "center" }}>
-                            <Button variant="contained" color="primary" style={{ textAlign: "center", justifyContent: "center" , marginTop:"60%"}}>
-                                Format JSON
-                            </Button>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={4} spacing={1} className={styles.GridRoot3}>
-                        <Paper className={styles.PaperStyle}>Formatted JSON</Paper>
-                    </Grid>
-                </Grid>
-            </>
-        );
+    const [jsonRawData, setRawData] = useState("");
+
+    const onChangeHandler = (e) => {
+        e.persist();
+        if (e.target && e.target.value) {
+            setRawData(jsonRawData => e.target.value);
+        }
+
     }
+    return (
+        <>
+            <Grid container spacing={1} direction="row"
+                justify="space-around"
+                alignItems="stretch">
+                <Grid item xs={4} spacing={1} className={styles.GridRoot1}>
+                    <Paper className={styles.PaperStyle}><br />
+                        <TextField className={styles.InputStyle} id="outlined-basic" label="Raw JSON" variant="outlined" multiline="true" onChange={(e) => onChangeHandler(e)} onBlur={(e) => onChangeHandler(e)} value={jsonRawData} />
+                        {/* <TextareaAutosize aria-label="minimum height" className={styles.InputStyle} rowsMin={35} placeholder="Minimum 3 rows" /> */}
+
+                    </Paper>
+                </Grid>
+                <Grid item xs={4} spacing={1} className={styles.GridRoot2}>
+                    <Paper className={styles.PaperStyle} style={{ textAlign: "center", justifyContent: "center" }}>
+                        <Button variant="contained" color="primary" style={{ textAlign: "center", justifyContent: "center", marginTop: "60%" }}>
+                            Format JSON
+                            </Button>
+                    </Paper>
+                </Grid>
+                <Grid item xs={4} spacing={1} className={styles.GridRoot3}>
+                    <Paper className={styles.PaperStyle}>Formatted JSON
+                    <br />
+                        {jsonRawData}
+                    </Paper>
+                </Grid>
+            </Grid>
+        </>
+    );
+
 
 
 
