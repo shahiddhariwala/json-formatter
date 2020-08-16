89521b3eb07a87d560da2fc9b0cbc54b12264132
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-08-16 23:08:39 +0530

    JSON objects key grouping

--- json_formatter/src/component/JsonDiplay.js
+++ json_formatter/src/component/JsonDiplay.js
@@ -1,5 +1,40 @@
 import React from 'react';
-const getJsonIndented = (obj) => JSON.stringify(obj, null, 4)
+const getJsonIndented = (obj) => {
+    console.log("JSON Data",obj);
+    for(let key in obj)
+    {
+        console.log(key);
+        if(typeof obj[key]==="string")
+        {
+            console.log("Its a String");
+        }
+        else if(typeof obj[key]==="number")
+        {
+            console.log("it a number");
+        }
+        else if(typeof obj[key]==="object")
+        {
+            if(Array.isArray(obj[key]))
+            {
+                console.log("it an Array");
+            }
+            else{
+                console.log("it a Object");
+            }
+        }
+        else if(typeof obj[key]==="function")
+        {
+            console.log("its a function");
+        }
+        else if(typeof obj[key]==="undefined")
+        {
+            console.log("its a undefined");
+        }
+    }
+    return JSON.stringify(obj, null, 4);
+
+}
+
 
 const JSONDisplayer = ({children}) => (
     <div>
